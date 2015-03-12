b1745d1d217f02f2e7a0c19cadf57706b454e406
Koen Karsten <koen.karsten@hva.nl>
2015-03-12 11:45:15 +0100

    [updated] Main now uses BoyerMoore

--- practicum3/src/practicum3/Practicum3.java
+++ practicum3/src/practicum3/Practicum3.java
@@ -5,6 +5,12 @@
  */
 package practicum3;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  *
  * @author koen_karsten
@@ -14,8 +20,14 @@ public class Practicum3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        String gedicht   = readFile("/Users/koen_karsten/Documents/Hva/S&S/Sorting-Searching-assignment-3/practicum3/src/practicum3/gedicht.txt");
+        BoyerMoore boyerMoore = new BoyerMoore(gedicht);
+    }
+    
+    static String readFile(String path) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, StandardCharsets.UTF_8);
     }
     
 }
